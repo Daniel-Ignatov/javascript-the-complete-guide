8a25829e22f501ac2e080d6aad994e12db8e7de1
SOFTSERVE\digna <digna@softserveinc.com>
2022-02-28 11:25:07 +0200

    completed first sections

---
+++ 34- Typescript/app.js
@@ -0,0 +1,39 @@
+var num1Input = document.getElementById('num1');
+var num2Input = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
+function add(a, b) {
+    return a + b;
+}
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+;
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    else if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
+}
+var results = [];
+var names = ['Max'];
+buttonElement.addEventListener('click', function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = add(num1, num2);
+    var resultContainer = {
+        res: result,
+        print: function () {
+            console.log(this.res);
+        }
+    };
+    results.push(resultContainer);
+    // results.push(5);
+    // results[0].print();
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
+    // printResult(result, 'window');
+});
---
+++ 34- Typescript/app.ts
@@ -0,0 +1,46 @@
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button');
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+type PrintMode = 'console' | 'alert';
+enum OutputMode { CONSOLE, ALERT };
+
+function printResult(result: string | number, printMode: OutputMode) {
+  if (printMode === OutputMode.CONSOLE) {
+    console.log(result);
+  } else if (printMode === OutputMode.ALERT) {
+    alert(result);
+  }
+}
+
+// const result = add(5, 3);
+// let isDone = false;
+
+// printResult(result);
+
+type CalculationResults = { res: number; print: () => void }[];
+
+const results: CalculationResults = [];
+const names = ['Max'];
+
+buttonElement.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  const resultContainer = {
+    res: result,
+    print() {
+      console.log(this.res);
+    }
+  };
+  results.push(resultContainer);
+  // results.push(5);
+  // results[0].print();
+  printResult(result, OutputMode.CONSOLE);
+  printResult(result, OutputMode.ALERT);
+  // printResult(result, 'window');
+});
